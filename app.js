9b6e979ded99b7da2dc9816f16503f05410b226e
GrayFawkes <gray.fawkes@outlook.com>
2020-04-26 23:37:52 +0200

    first push
    first push of the server

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');   // Require express
+// var mongoose = require('mongoose');
+
+var app = express();    // Instanciate Express application
+
+app.get('/', (req, res) => {	// Function executed at root of the page
+	res.send('openPharma 0.1')
+});
+
+console.log('openPharma launched on port 3000');	// Test
+app.listen(3000);
