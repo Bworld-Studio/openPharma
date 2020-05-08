c26a077e1f01f5f81f98bb190e1606717123a32d
GrayFawkes <gray.fawkes@outlook.com>
2020-05-08 12:18:25 +0200

    0.1

---
+++ app/app.js
@@ -0,0 +1,14 @@
+// var express = require('express');		// Require Express
+// // var mongoose = require('mongoose');	// Require mongoose
+// var vue = require('vue');
+
+// // mongoose.connect('mongodb://localhost/openpharma')
+// // 				.then(() =>  console.log('connection successful'))
+// // 				.catch((err) => console.error(err));
+
+// var app = express();		// Instanciate Application
+
+// app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // Get Boostrap.css
+
+// console.log("openPharma launched on port 3000");
+// app.listen(3000);
