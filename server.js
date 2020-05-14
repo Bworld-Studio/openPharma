11ae6090a46931129c4861e41fb3cab443873f0e
GrayFawkes <gray.fawkes@outlook.com>
2020-05-14 22:49:38 +0200

    0.1

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+	host: 'mydb.com',
+	user:'myUser',
+	password: 'myPassword',
+	connectionLimit: 0
+});
+
 const vue = require('vue');
 
 const app = express();
