b5fdf9be2b16e2afc1a0edf4b68959d5152b1b86
Gray Fawkes <gray.fawkes@outlook.com>
2020-06-15 18:57:27 +0200

    0.1.1

--- routes/clients.js
+++ routes/clients.js
@@ -44,26 +44,43 @@ router.delete("/client/:uuid", (req, res) => {
 });
 
 // Update Client
-router.update("/Client/:uuid", (req, res) => {
-	Client.update(req.body, {
-		where: { uuid: req.params.uuid }
-	})
-		.then(num => {
-			if (num == 1) {
-				res.send({
-					message: "Client was updated successfully."
-				});
-			} else {
-				res.send({
-					message: `Cannot update Client with id=${uuid}. Maybe Client was not found or req.body is empty!`
-				});
-			}
+router.put("/client/:uuid", (req, res) => {
+	if (!req.body.numSS) {
+		res.status(400);
+		res.json({
+			error: "Bad Data"
 		})
-		.catch(err => {
-			res.status(500).send({
-				message: "Error updating Client with id=" + uuid
-			});
-		});
-});
+	} else {
+		Client.update(
+			req.body,
+			{ where: { uuid: req.params.uuid } }
+		)
+			.then(() => {
+				res.send("Task Updated")
+			})
+			.error(err => res.send(err))
+	}
+})
+// router.update("/Client/:uuid", (req, res) => {
+// 	Client.update(req.body, {
+// 		where: { uuid: req.params.uuid }
+// 	})
+// 		.then(num => {
+// 			if (num == 1) {
+// 				res.send({
+// 					message: "Client was updated successfully."
+// 				});
+// 			} else {
+// 				res.send({
+// 					message: `Cannot update Client with id=${uuid}. Maybe Client was not found or req.body is empty!`
+// 				});
+// 			}
+// 		})
+// 		.catch(err => {
+// 			res.status(500).send({
+// 				message: "Error updating Client with id=" + uuid
+// 			});
+// 		});
+// });
 
 module.exports = router;
