432127ad94d243ccad7537c8ab6220a3b75e72b3
Gray Fawkes <gray.fawkes@outlook.com>
2021-04-15 10:38:24 +0200

    :hammer: Correction of the backend status API

---
+++ src/routes/api/status/status.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+// const Template = require('../models/Template')
+
+// Get orders
+router.get('/status', (req, res) => {
+	res.json(200)
+})
+
+module.exports = router
